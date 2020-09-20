8bad36f9e8fc7b8e5eb4d45fc6358405fc69baae
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-09-20 20:53:58 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Random random = Random();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan Animated Container"),
+        ),
+        body: Center(
+          child: myGestureDetector(),
+        ),
+      ),
+    );
+  }
+
+  GestureDetector myGestureDetector() {
+    return GestureDetector(
+      onTap: () {
+        setState(() {});
+      },
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 500),
+        width: 50.0 + random.nextInt(150),
+        height: 50.0 + random.nextInt(200) / 1 + random.nextInt(3),
+        color: Color.fromARGB(
+          255,
+          random.nextInt(256),
+          random.nextInt(256),
+          random.nextInt(256),
+        ),
+      ),
+    );
+  }
+}
